93d8c104c753cf867bffdb6bf7f075fe26ba073a
Boubakri Nawres <47699352+nawresboubakri@users.noreply.github.com>
2019-12-04 21:44:02 +0100

    Add files via upload

---
+++ Nawres Boubakri  Pro/intégration nawres/stafft.h
@@ -0,0 +1,47 @@
+#ifndef STAFFT_H
+#define STAFFT_H
+
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QDate>
+
+class stafft
+{
+public:
+    stafft();
+    stafft(QString,QString,QString,QString,QDate,QString,int,int,int,int,int,QString);
+    QString get_occupation();
+    QString get_role();
+    QString get_nom();
+    QString get_prenom();
+    QDate get_date();
+    QString get_sexe();
+    QString get_mail();
+
+    int get_cin();
+    int get_tel();
+    int get_salaire();
+    int get_contrat();
+    int get_horaire();
+    bool ajouter();
+    bool modifier(QString,QString,QString,QString,QString,QDate,QString,QString,QString,QString,QString,QString,QString);
+    int stcalM() ;
+    int stcalF() ;
+    QSqlQueryModel * trier1 () ;
+    QSqlQueryModel * trier2 () ;
+    QSqlQueryModel * trier3 () ;
+    QSqlQueryModel * trier4 () ;
+    QSqlQueryModel * trier5 () ;
+    QSqlQueryModel * trier6 () ;
+    QSqlQuery recherche1(QString) ;
+    QSqlQuery recherche2(QString) ;
+    QSqlQueryModel * afficher();
+    bool supprimer(int);
+ private:
+     QString occupation,role,nom,prenom,sexe,mail ;
+     QDate date ;
+     int CIN,tel,salaire,contrat,horaire ;
+};
+
+#endif // STAFFT_H
